327df84779a39a604f4a633d21578b228f450729
truelq <321259693@qq.com>
2019-10-12 22:55:30 +0800

    1

--- DP/P2478.cpp
+++ DP/P2478.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <vector>
 #include <stack>
-#include<unordered_set>
 #include <algorithm>
 using namespace std;
 
@@ -10,66 +9,48 @@ int main()
 {
     int n;
     cin >> n;
-    int ss[n + 1];
+    int ss[n + 2];
+    int s[n + 2];
     for (int i = 1; i <= n; ++i)
     {
-        scanf("%d", ss + i);
+        scanf("%d %d", ss + i, s + i);
     }
-    ss[0]=0x7fffffff;
-    int save[n + 1] = {0};
+    ss[0] = 0x7fffffff;
+    s[0] = 0x7fffffff;
+    ss[n+1]=0;
+    s[n+1]=0;
+    int save[n + 2] = {0};
     vector<int> source[n + 2];
+    double count[n+2]={0};
     int maxs = 0;
-    for (int i = 1; i <= n; ++i)
+    count[0]=1;
+    for (int i = 1; i <= n+1; ++i)
     {
         int temp = 0;
         for (int j = 0; j < i; ++j)
         {
-            if (ss[i] < ss[j])
+            if (ss[i] <= ss[j] && s[i] <= s[j])
                 temp = max(temp, save[j]);
         }
         for (int j = 0; j < i; ++j)
         {
-            if (ss[i] < ss[j] && save[j] == temp)
+            if (ss[i] <= ss[j] && s[i] <= s[j] && save[j] == temp)
             {
                 source[i].push_back(j);
+                count[i]+=count[j];
             }
         }
         save[i] = temp + 1;
         maxs = max(maxs, save[i]);
     }
-    cout<<maxs<<' ';
+    cout << maxs-1 << endl;
     int temp = 0;
-    int ans=0;
-    stack<int> sss;
-    unordered_set<int> ssss;
-    for (int i = 1; i <= n; ++i)
-    {
-        if (maxs == save[i])
-        {
-            source[n+1].push_back(i);
-        }
-    }
-    ssss.clear();
-    for(int i=source[n+1].size()-1;i>=0;--i){
-        if(ssss.find(ss[source[n+1][i]])==ssss.end()){
-            sss.push(source[n+1][i]);
-            ssss.insert(ss[source[n+1][i]]);
-        }
-    }
-    while(!sss.empty()){
-         temp=sss.top();
-         sss.pop();
-         ssss.clear();
-         if(source[temp].size()==0){
-            ++ans;
-         }
-         for(int i=source[temp].size()-1;i>=0;--i){
-             if(ssss.find(ss[source[temp][i]])==ssss.end()){
-                 sss.push(source[temp][i]);
-                 ssss.insert(ss[source[temp][i]]);
-             }
-         }
+    double ans = count[n+1];
+    
+    for(int i=1;i<=n;++i){
+        count[i]/=ans;
+        cout<<count[i]<<' ';
     }
 
-    cout << ans << endl;
+    //cout << ans << endl;
 }
