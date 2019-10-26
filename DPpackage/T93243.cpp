e763faa6ec8dda335696552b5009f0354854d7c2
truelq <321259693@qq.com>
2019-10-26 13:33:30 +0800

    最大报销额

--- DPpackage/T93243.cpp
+++ DPpackage/T93243.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //最大报销额 多重限制
 // https://www.luogu.org/problem/T93243
 int save[3000050];
+int sss[3000050];
 int main() {
   double w = 0;
   int n = 0;
@@ -11,34 +12,35 @@ int main() {
   int flag;
   char c;
   double v;
+  int count=0;
   double maxs = 0;
   double ss[26];
-  vector<int> sss;
+  //vector<int> sss;
   while (scanf("%lf %d", &w, &n) && n) {
-    sss.clear();
+    //sss.clear();
+    count=0;
     for (int i = 0; i < n; ++i) {
       ss[0] = ss[1] = ss[2] = 0;
       maxs = 0;
       flag = 0;
       scanf("%d", &t);
       for (int i = 0; i < t; ++i) {
-        getchar();
-        scanf("%c:%lf", &c, &v);
+        scanf(" %c:%lf", &c, &v);
         if (c == 'X') {
           flag=1;
-          continue;
         }
         ss[c - 'A'] += v;
       }
       if (flag||ss[0]>600||ss[1]>600||ss[2]>600||ss[0]+ss[1]+ss[2]>1000) {
         continue;
       }
-      sss.push_back(round(maxs * 100));
+      //sss.push_back(round((ss[0]+ss[1]+ss[2]) * 100));
+      sss[count++]=round((ss[0]+ss[1]+ss[2]) * 100);
     }
     // sort(sss.begin(), sss.end());
     int length = round(w * 100);
     memset(save, 0, sizeof(save));
-    for (int i = 0; i < sss.size(); ++i) {
+    for (int i = 0; i < count; ++i) {
       for (int j = length; j >= sss[i]; --j) {
         save[j] = max(save[j], save[j - sss[i]] + sss[i]);
       }
