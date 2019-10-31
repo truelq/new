4fb511a7d8467397e3c4e9acf145615dbe88d8e0
truelq <321259693@qq.com>
2019-10-31 11:31:21 +0800

    并查集

--- bingchaji/P3958.cpp
+++ bingchaji/P3958.cpp
@@ -4,12 +4,12 @@
 // https://www.luogu.org/problem/P3958
 using namespace std;
 struct P3958 {
-  unsigned long long x;
-  unsigned long long y;
-  unsigned long long z;
+  long long x;
+  long long y;
+  long long z;
 };
 
-P3958 ss[10005];
+P3958 ss[100005];
 bool operator<(P3958 &a, P3958 &b) {
   if (a.z == b.z) {
     if (a.y == b.y) {
@@ -19,34 +19,79 @@ bool operator<(P3958 &a, P3958 &b) {
   }
   return a.z < b.z;
 }
-int sets[10005];
-int par[10005];
+int sets[100005];
+int par[100005];
 void makeset(int x, int *sets, int *par);
 int findset(int x, int *par);
 void unionset(int x, int y, int *sets, int *par);
-
+int savea[100005];
+int saveb[100005];
+int acount = 0;
+int bcount = 0;
 int main() {
+  freopen("C:\\Users\\32125\\Documents\\vscodec\\bingchaji\\testdata.in", "r",
+          stdin);
   int t;
   int n, h, r;
   cin >> t;
   while (t--) {
     memset(ss, 0, sizeof(ss));
+    acount = 0;
+    bcount = 0;
     cin >> n >> h >> r;
-    makeset(n, sets, par);
+    for (int i = 1; i <= n; ++i)
+      makeset(i, sets, par);
     for (int i = 1; i <= n; ++i) {
-      scanf("%d %d %d", &ss[i].x, &ss[i].y, &ss[i].z);
+      scanf("%lld %lld %lld", &ss[i].x, &ss[i].y, &ss[i].z);
     }
     sort(ss + 1, ss + n + 1);
     if (ss[1].z > r || ss[n].z + r < h) {
       printf("No\n");
     } else {
       for (int i = 1; i <= n; ++i) {
+        if (ss[i].z <= r) {
+          savea[acount++] = i;
+        }
+        if (ss[i].z + r >= h) {
+          saveb[bcount++] = i;
+        }
+        for (int j = i + 1; j <= n; ++j) {
+          unsigned long long x = (ss[j].x - ss[i].x) * (ss[j].x - ss[i].x);
+          unsigned long long y = (ss[j].y - ss[i].y) * (ss[j].y - ss[i].y);
+          unsigned long long z = (ss[j].z - ss[i].z) * (ss[j].z - ss[i].z);
+          if (sqrt(x) <= 2 * r && sqrt(x + y) <= 2 * r &&
+              sqrt(x + y + z) <= 2 * r) {
+            int x = findset(i, par);
+            int y = findset(j, par);
+            if (x != y)
+              unionset(i, j, sets, par);
+          }
+        }
+      }
+
+      int flag = 0;
+      for (int i = 0; i < acount; ++i) {
+        for (int j = 0; j < bcount; ++j) {
+          if (findset(savea[i], par) == findset(saveb[j], par)) {
+            flag = 1;
+
+            break;
+          }
+        }
+        if (flag)
+          break;
+      }
+      if (flag) {
+        printf("Yes\n");
+      } else {
+        printf("No\n");
       }
     }
   }
+  return 0;
 }
 void makeset(int x, int *sets, int *par) {
-  sets[x] = 1;
+  sets[x] = 0;
   par[x] = x;
 }
 int findset(int x, int *par) {
---
+++ segmentTree/P3740.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+// https://www.luogu.org/problem/P3740
+struct Node {
+  int l, r;
+  long long sum;
+  long long inc;
+
+  int mid() { return (l + r) >> 1; }
+};
+struct Tree {
+  Node tree[400004]; // 4n
+  int root = 1;
+  int end;
+  void buildtree(int root, int l, int r, long long *ss) {
+    this->tree[root].l = l;
+    this->tree[root].r = r;
+    if (l == r) {
+      tree[root].sum = ss[l];
+      return;
+    }
+    buildtree(root << 1, l, tree[root].mid(), ss);
+    buildtree((root << 1) + 1, tree[root].mid() + 1, r, ss);
+    tree[root].sum = tree[root << 1].sum + tree[(root << 1) + 1].sum;
+  }
+  void update(int root, int l, int r, long long k) {
+    if (l == tree[root].l && r == tree[root].r) {
+      this->tree[root].sum = (r - l + 1) * k; //只要是1就可以了
+      this->tree[root].inc |= k;
+      return;
+    }
+    down(root);
+    if (l > this->tree[root].mid()) {
+      update((root << 1) + 1, l, r, k);
+    } else if (r <= this->tree[root].mid()) {
+      update(root << 1, l, r, k);
+    } else {
+      update(root << 1, l, this->tree[root].mid(), k);
+      update((root << 1) + 1, this->tree[root].mid() + 1, r, k);
+    }
+    this->tree[root].sum = tree[root << 1].sum + tree[(root << 1) + 1].sum;
+  }
+  long long query(int root, int l, int r) {
+    if (l == tree[root].l && r == tree[root].r) {
+      return tree[root].sum;
+    }
+    down(root);
+    long long ans = 0;
+    if (l > tree[root].mid()) {
+      ans = query((root << 1) + 1, l, r);
+    } else if (r <= tree[root].mid()) {
+      ans = query((root << 1), l, r);
+    } else {
+      ans = query(root << 1, l, tree[root].mid()) +
+            query((root << 1) + 1, tree[root].mid() + 1, r);
+    }
+    return ans;
+  }
+  void down(int root) {
+    if (tree[root].inc) {
+      update(root << 1, tree[root << 1].l, tree[root << 1].r, tree[root].inc);
+      update((root << 1) + 1, tree[(root << 1) + 1].l, tree[(root << 1) + 1].r,
+             tree[root].inc);
+
+      // tree[root].inc = 0;
+    }
+  }
+} tree;
+struct P3740 {
+  int x;
+  int y;
+};
+bool operator<(P3740 &a, P3740 &b) { return a.x < b.x; }
+using namespace std;
+
+P3740 ss[1001];
+P3740 save[2002];
+// long long state[2002];
+long long states[4001];
+map<int, int> state;
+int main() {
+  int n, m;
+  cin >> n >> m;
+  int count = 1;
+  for (int i = 1; i <= m; ++i) {
+    scanf("%d %d", &ss[i].x, &ss[i].y);
+    save[count].x = ss[i].x;
+    save[count].y = count;
+    count++;
+    save[count].x = ss[i].y;
+    save[count].y = count;
+    count++;
+  }
+  tree.end = 4000;
+  tree.buildtree(1, 1, 4000, states);
+  sort(save + 1, save + count);
+  int statecount = 1;
+  for (int i = 1; i < count; ++i) {
+    state[save[i].y] = statecount;
+    if (i + 1 < count) {
+      if (save[i].x + 1 < save[i + 1].x) {
+        statecount += 2;
+      } else if (save[i].x < save[i + 1].x) {
+        statecount += 1;
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = m; i; --i) {
+    if (state[i << 1] - state[(i << 1) - 1] + 1 >
+        tree.query(1, state[(i << 1) - 1], state[i << 1])) {
+      ans += 1;
+    }
+    tree.update(1, state[(i << 1) - 1], state[i << 1], 1);
+  }
+  cout << ans << endl;
+  return 0;
+}
